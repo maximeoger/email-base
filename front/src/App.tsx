bfebd0855b6cc041a097c221d9643d04b381474e
maximeoger <maximeoger93@proton.me>
2023-07-06 20:23:20 +0200

    [front] get emails from backend

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/mails?min=1&max=10')
+        const data = await response.json()
+        setData(data)
+        setLoading(false)
+      } catch (err) {
+        setLoading(false)
+        setError(true)
+        console.log(err)
+      }
+    }
+
+    fetchData().catch(error => {
+      console.log(error)
+    })
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="mx-auto px-2 sm:px-4 lg:px-8">
+        <span className="leading-9">e-mail base</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="min-h-full bg-gray-100 p-6">
+        <div className="mt-0 mx-auto w-fit grid gap-4 grid-cols-4">
+          {loading && (<p>Loading ...</p>)}
+          {error && (<p>Une erreur est survenue :(</p>)}
+          {
+            data.map(mail => (
+              <div className="w-64 h-96 rounded-lg bg-white">
+                <p>{mail.subject}</p>
+                <img
+                  className="w-64 h-32 object-cover object-top"
+                  src={`data:image/jpg;base64,${mail.screenshot}`} alt={mail.subject}
+                />
+              </div>
+            ))
+          }
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
