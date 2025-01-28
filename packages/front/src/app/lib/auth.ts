ef10cbe4987c5af00dc71f139063af6400696192
maximeoger <maximeoger93@proton.me>
2025-01-28 13:34:49 +0100

    Revoke google OAuth token on signOut

--- packages/front/src/app/lib/auth.ts
+++ packages/front/src/app/lib/auth.ts
@@ -2,7 +2,8 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import JWT from "jsonwebtoken";
 
-const authOptions = {
+//@ts-ignore
+export const { auth, handlers } = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -10,19 +11,45 @@ const authOptions = {
     }),
   ],
   callbacks: {
-    async session({ session, token }: any) {
+    //@ts-ignore
+    async jwt ({ token, account }) {
+
+      if (account?.access_token) {
+        token.accessToken = account.access_token;
+      }
+
+      return token
+
+    },
+    //@ts-ignore
+    async session({ session, token }) {
+
+      const { accessToken, ...rest } = token;
+
+      session.accessToken = accessToken;
+
       session.user.jwt = JWT.sign(
-        { data: token },
+        { data: rest },
         process.env.CUSTOM_JWT_SECRET as unknown as string,
       );
+
       return session;
     },
   },
+
+  events: {
+    //@ts-ignore
+    async signOut({ token }) {
+      const revokeUrl = `https://accounts.google.com/o/oauth2/revoke?token=${token.accessToken as string}`;
+      fetch(revokeUrl).catch(err => console.error("Error on google OAuth revocation", err))
+    }
+  },
+
   secret: process.env.NEXTAUTH_SECRET,
+  
   Session: {
     strategy: "jwt",
   },
+  
   debug: process.env.NODE_ENV === "development",
-};
-
-export const { auth, handlers } = NextAuth(authOptions);
+});
